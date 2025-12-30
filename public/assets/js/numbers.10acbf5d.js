c5e534fcf0d17a4c8e55db336f7255ce2eb4e90a
NGUYEN VAN DAT <n_dat@cyllenge.com>
2025-12-30 08:48:26 +0700

    update

--- public/assets/js/numbers.10acbf5d.js
+++ public/assets/js/numbers.10acbf5d.js
@@ -2,17 +2,19 @@ const container = document.getElementById("items");
 const template = document.getElementById("item-template");
 const dialog = document.getElementById("dialog");
 
+// reuse a single audio player
+const audio = new Audio();
+audio.preload = "auto";
+
 fetch("assets/data/numbers.json")
     .then((res) => res.json())
     .then((data) => {
-        const container = document.getElementById("items");
-        const template = document.getElementById("item-template");
-
         data.items.forEach((item) => {
             const clone = template.content.cloneNode(true);
             const card = clone.querySelector(".card");
             const img = clone.querySelector(".item-image");
             const name = clone.querySelector(".item-name");
+
             img.src = `assets/images/numbers/${item.image[0]}`;
             img.alt = item.name;
             name.innerText = item.name;
@@ -20,25 +22,30 @@ fetch("assets/data/numbers.json")
             container.appendChild(clone);
 
             card.addEventListener("click", () => {
-                const name = dialog.querySelector(".item-name");
-                name.innerText = item.name;
-                const img = dialog.querySelector(".item-image");
-                img.src = `assets/images/numbers/${item.image[0]}`;
-                img.alt = item.name;
-                name.onclick = () => {
-                    const utterance = new SpeechSynthesisUtterance(item.name);
-                    speechSynthesis.speak(utterance);
+                const dialogName = dialog.querySelector(".item-name");
+                const dialogImg = dialog.querySelector(".item-image");
+
+                dialogName.innerText = item.name;
+                dialogImg.src = `assets/images/numbers/${item.image[0]}`;
+                dialogImg.alt = item.name;
+
+                dialogName.onclick = () => {
+                    audio.src = `assets/sounds/numbers/${item.sound[0]}`;
+                    audio.currentTime = 0;
+                    audio.play().catch((err) => {
+                        console.error("Audio play failed:", err);
+                    });
                 };
+
                 dialog.classList.remove("hidden");
             });
         });
     })
     .catch((err) => console.error("Error loading data:", err));
 
-// Close dialog when clicking outside the content
+// Close dialog when clicking outside content
 dialog.addEventListener("click", (e) => {
     if (e.target === dialog) {
-        // only if clicked on the overlay
         dialog.classList.add("hidden");
     }
 });
